82ba478fb7a2e13b4d22b9c833918970f740fdc9
Maxime BOUCHER-AGUILAR <m.boucheraguilar@campus-igs-toulouse.fr>
2019-12-17 09:32:15 +0100

    Ajoutez des fichiers projet.

---
+++ DataBinding1/Artiste.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBinding1
+{
+    class Artiste
+    {
+        public string nom;
+        public List<Chanson> listChanson = new List<Chanson>();
+        
+        public Artiste(string nom)
+        {
+            this.nom = nom;
+        }
+    }
+}
---
+++ DataBinding1/Chanson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBinding1
+{
+    public class Chanson
+    {
+        public string nomArtiste;
+        public string titreChanson;
+
+        public Chanson(string nomArtiste, string titreChanson)
+        {
+            this.nomArtiste = nomArtiste;
+            this.titreChanson = titreChanson;
+        }
+    }
+}
